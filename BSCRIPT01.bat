4c6b6b5e9c38671d37515fa27fee1ef23122ac60
unknown <dilanjohndrell@gmail.com>
2024-07-12 17:22:00 +0800

    Initial commit with BSCRIPT01.bat

---
+++ BSCRIPT01.bat
@@ -0,0 +1,9 @@
+@echo off
+start chrome "https://uc-bcf.instructure.com"
+start chrome "https://www.netacad.com"
+start chrome "https://www.google.com"
+start chrome "https://www.youtube.com"
+start chrome "https://www.facebook.com"
+start calc
+start notepad
+shutdown /s /t 60
